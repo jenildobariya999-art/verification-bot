37ad74977141a4746541f9cd538dc24d5a1bbfe9
jenildobariya999-art <jenildobariya999@gmail.com>
2026-03-27 16:41:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import hashlib, json, os, threading
 API_TOKEN = os.environ.get("API_TOKEN")
 DOMAIN = "https://verification-beta-five.vercel.app"
 
-ADMIN_IDS = ["123456789"]  # CHANGE
+ADMIN_IDS = ["6925391837"]  # CHANGE
 bot_status = True
 
 # ===== INIT =====
